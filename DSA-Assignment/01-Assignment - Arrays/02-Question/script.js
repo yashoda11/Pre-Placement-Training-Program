e72cb76f90b5e3c64185b583880bb5b7e30cec3a
Yashoda <yashodalakshmi999@gmail.com>
2023-05-27 14:28:36 +0530

    Assignment Questions 1, 2 and 3 are added

--- DSA-Assignment/01-Assignment - Arrays/02-Question/script.js
+++ DSA-Assignment/01-Assignment - Arrays/02-Question/script.js
@@ -1,11 +1,19 @@
-var twoSum = function(nums, target) {
-    for(let i = 0; i < nums.length; i++){
-        for(let j = i+1; j < nums.length; j++){
-            if(nums[i] + nums[j] == target){
-                return [i, j]
-            }
-        }
+var removeOccrances = function (nums, val){
+    let count = 0;
+    for(let i = 0;i<nums.length;i++){
+      if(nums[i]!==val){
+          nums[count++];
+          }
+      }
+    return count;
     }
-};
-console.log(`The Output of the Indices are :[ ${twoSum([2, 7, 11, 15],9)}]`);
-console.log(`The Output of the Indices are :[ ${twoSum([3, 2, 4],6)}]`);
+  
+  var nums = [3,2,2,3,4,6,8,3,11,7,9];
+  var val = 3;
+  const result = nums.filter(n => n!==3);
+  
+  console.log(`
+  After removing all Occurances of value (${val}) in the given array [${nums}],the remaining elements are : 
+  ${removeOccrances(nums,val)} elements and 
+  The New Array is: [${result}]`);
+  
