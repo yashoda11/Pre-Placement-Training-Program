453f84a4f3dc741a11fad9863e30b4468991c7cb
Yashoda <yashodalakshmi999@gmail.com>
2023-05-28 13:08:39 +0530

    Assignment Questions 5 and 6 added

--- DSA-Assignment/01-Assignment - Arrays/05-Question/script.js
+++ DSA-Assignment/01-Assignment - Arrays/05-Question/script.js
@@ -1,30 +1,13 @@
-var array1 = [1,2,3,0,0,0];
-var array2 = [4,5,6];
-
-// var merg = function(array1,array2){
-//     var arr = [];
-//     var j = 0;
-//     var i = 0;
-//     while(i < array1.length && j < array2.length){
-//         if(array1[i] < array2[j]){
-//             arr.push(array1[i]);
-//             i++;
-//         }
-//         else{
-//             arr.push(array2[j]);
-//             j++;
-//         }
-//     }
-//     while(j < array2.length){
-//         arr.push(array2[j]);
-//         j++;
-//     }
-//     return arr;
-// }
-// var array = merg(array1,array2);
-// console.log(array);
-
-
-var merg = array1.concat(...array2);
-
-console.log(merg);
+var merge = function(nums1, m, nums2, n) {
+    for (let i = m; i <= m + n; i++) {
+      if (nums1[i] === 0) {
+        nums1[i] = nums2[i - m]
+      }
+    }
+    nums1.sort((a,b) => a-b)
+    return nums1;
+  };
+  
+  console.log(`The Output is: [${merge([1,2,3,0,0,0], 3, [2,5,6], 3)}]`);
+   
+  
