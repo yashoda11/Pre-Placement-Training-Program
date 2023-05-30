8dca11c6dc36429e0edd08f99642ebda93b2974c
Yashoda <yashodalakshmi999@gmail.com>
2023-05-30 14:48:43 +0530

    Question 1 code updated

--- DSA-Assignment/02-Assignment - Arrays/01-Question/script.js
+++ DSA-Assignment/02-Assignment - Arrays/01-Question/script.js
@@ -1,11 +1,11 @@
-var twoSum = function(nums, target) {
-    for(let i = 0; i < nums.length; i++){
-        for(let j = i+1; j < nums.length; j++){
-            if(nums[i] + nums[j] == target){
-                return [i, j]
-            }
-        }
+var arrayPairSum = function(nums) {
+    nums.sort((a, b) => a - b);
+    let sum = 0;  
+
+    for(let i = 0; i < nums.length; i += 2) {
+        sum += nums[i];
     }
+
+    return `The Maximum Possible Sum for the given array is: ${sum}`;
 };
-console.log(`The Output of the Indices are :[ ${twoSum([2, 7, 11, 15],9)}]`);
-console.log(`The Output of the Indices are :[ ${twoSum([3, 2, 4],6)}]`);
+console.log(arrayPairSum([1,4,3,2]));
