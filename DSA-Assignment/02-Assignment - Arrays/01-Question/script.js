f413aa301576f3a43d4701438f4e256675f865cb
Yashoda <yashodalakshmi999@gmail.com>
2023-05-30 15:09:49 +0530

    Question 2 Added

--- DSA-Assignment/02-Assignment - Arrays/01-Question/script.js
+++ DSA-Assignment/02-Assignment - Arrays/01-Question/script.js
@@ -1,11 +1,10 @@
-var arrayPairSum = function(nums) {
+var arrayPairMaxPossibleSum = function(nums) {
+    console.log(`OUTPUT: \nThe given Array is: [${nums}]`);
     nums.sort((a, b) => a - b);
     let sum = 0;  
-
     for(let i = 0; i < nums.length; i += 2) {
         sum += nums[i];
     }
-
     return `The Maximum Possible Sum for the given array is: ${sum}`;
 };
-console.log(arrayPairSum([1,4,3,2]));
+console.log(arrayPairMaxPossibleSum([1,4,3,2]));
