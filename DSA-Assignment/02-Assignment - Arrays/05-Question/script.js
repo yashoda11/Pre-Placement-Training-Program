948e7407edd3324357a66818ad5c8821097797c1
Yashoda <yashodalakshmi999@gmail.com>
2023-05-31 09:44:57 +0530

    Question 5 Updated and Question 6 added

--- DSA-Assignment/02-Assignment - Arrays/05-Question/script.js
+++ DSA-Assignment/02-Assignment - Arrays/05-Question/script.js
@@ -1,8 +1,9 @@
 var maxProduct = function(nums){
     let n = nums.length;
     nums.sort((a,b) => (a-b));
-    // maxProduct = nums[0]*nums[1]*nums[nums.length-1];
-    console.log(nums);
-    console.log(nums[n-1]*nums[n-2]*nums[n-3]);
+    console.log(`
+    The Maximum Product is : ${nums[n-1]*nums[n-2]*nums[n-3]}
+             `);
 };
-maxProduct([-10, -3, -5, -6, -20]);
+var nums = [1,2,3];
+maxProduct(nums);
