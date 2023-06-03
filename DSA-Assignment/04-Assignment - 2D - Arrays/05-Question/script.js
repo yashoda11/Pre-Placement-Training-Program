637995f43c1a5a100834c8c8939e51b08ce9a3fe
Yashoda <yashodalakshmi999@gmail.com>
2023-06-03 09:19:59 +0530

    Question 5 added and updated its code

---
+++ DSA-Assignment/04-Assignment - 2D - Arrays/05-Question/script.js
@@ -0,0 +1,20 @@
+function numberOfCompleteRows(n) {
+    let completeRows = 0;
+    let coins = 0;
+    let i = 1;
+  
+    while (coins + i <= n) {
+      coins += i;
+      completeRows++;
+      i++;
+    }
+  
+    return completeRows;
+  }
+  
+  // Example usage:
+  let n = 5;
+  console.log(`
+  The Number of completed rows for the ${n} coins are : ${numberOfCompleteRows(n)}
+  `);
+  
