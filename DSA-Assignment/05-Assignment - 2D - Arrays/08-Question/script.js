f8d83b9b4b1afb6165bd47e7b9005ce593b95ee4
Yashoda Viyyapu <121178061+yashoda11@users.noreply.github.com>
2023-06-04 20:31:16 +0530

    Question 8 Updated

--- DSA-Assignment/05-Assignment - 2D - Arrays/08-Question/script.js
+++ DSA-Assignment/05-Assignment - 2D - Arrays/08-Question/script.js
@@ -1,4 +1,12 @@
 var findDoubleArray = function (changed) {
+    console.log(`
+        The given double Array is : [${changed}]`);;
+    if (changed.length % 2 !== 0) {
+        return `
+        The length of changed ${changed.length} is odd, it cannot be a doubled array.
+        hence [].
+        `;
+      }
     let nums = new Set();
     double = [];
     for ( let i = 0; i < changed.length; i ++) {
@@ -7,11 +15,9 @@ var findDoubleArray = function (changed) {
             double.push(changed[i]/2);
         }
     }
-    return double;
+    return `
+        The Original array is : [${double}]
+    `;
 };
 var changed = [1,3,4,2,6,8];
-console.log(`
-The Double Array is : [${changed}] ,
-
-The Original array is : [${findDoubleArray(changed)}]
-`);;
+console.log((findDoubleArray(changed)));
