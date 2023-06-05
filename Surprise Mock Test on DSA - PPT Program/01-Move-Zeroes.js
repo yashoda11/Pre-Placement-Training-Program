1073174b68e0d499d482643dc80b106d3a942fe2
Yashoda <yashodalakshmi999@gmail.com>
2023-06-05 15:57:54 +0530

    Mock Test Question added

---
+++ Surprise Mock Test on DSA - PPT Program/01-Move-Zeroes.js
@@ -0,0 +1,22 @@
+var moveAllZeorsToAtEnd = function (nums)
+{
+	let count = 0;
+	for (let i = 0; i < nums.length; i++) {
+		if (nums[i] == 0) {
+			count++;
+			nums.splice(i,1);
+			i--;
+		}
+	}
+	for (let i = 0; i < count; i++)
+	{
+		nums.push(0);
+	}
+	return `
+	The given array after shifting all zeros to right side is: [${nums}]
+	`;
+}
+var nums1 = [0,1,0,3,12];
+var nums2 = [0];
+console.log(moveAllZeorsToAtEnd(nums1));
+console.log(moveAllZeorsToAtEnd(nums2));
