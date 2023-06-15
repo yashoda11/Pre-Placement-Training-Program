54b88d5aa9cc8b52abad7952c30b786807b74120
Yashoda <yashodalakshmi999@gmail.com>
2023-06-15 08:01:58 +0530

    Assignment - 12 Question - 07 Updated

--- DSA-Assignment/12-Assignment - Linked List/07-Question/script.js
+++ DSA-Assignment/12-Assignment - Linked List/07-Question/script.js
@@ -83,24 +83,3 @@ console.log('First List:', list1.toString());
 
 console.log('Second List:', list2.toString());
 // Output: Second List: (empty)
-
-// Example 2
-const list3 = new LinkedList();
-list3.append(1);
-list3.append(2);
-list3.append(3);
-
-const list4 = new LinkedList();
-list4.append(4);
-list4.append(5);
-list4.append(6);
-list4.append(7);
-list4.append(8);
-
-list3.mergeAlternate(list4);
-
-console.log('First List:', list3.toString());
-// Output: First List: 1->4->2->5->3->6
-
-console.log('Second List:', list4.toString());
-// Output: Second List: 7->8
