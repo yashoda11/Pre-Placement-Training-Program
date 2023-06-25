4d3b24cfe57f51faf29f4ab43b7ac1aa4b78f914
yashoda11 <yashodalakshmi999@gmail.com>
2023-06-25 12:19:49 +0530

    Mock-Test Question Updated

--- Surprise Mock Test on DSA - PPT Program/Moct-Test - 03/Implement-Stack.js
+++ Surprise Mock Test on DSA - PPT Program/Moct-Test - 03/Implement-Stack.js
@@ -1,3 +1,9 @@
+// Mock Test - 03 :
+// Question - 01 :
+// Implement a Stack using an array in JavaScript. 
+// Include the necessary methods such as push, pop, and isEmpty.
+
+
 class Stack {
     constructor() {
       this.stackArray = [];
@@ -19,17 +25,26 @@ class Stack {
     }
   }
   
-  // Usage example
+// Testing Stack Operations :
+
   const myStack = new Stack();
   
+// 1. Push Operations :
+
   myStack.push(5);
   myStack.push(10);
   myStack.push(15);
   
-  console.log(myStack.pop()); // Output: 15
-  console.log(myStack.pop()); // Output: 10
-  console.log(myStack.isEmpty()); // Output: false
-  console.log(myStack.pop()); // Output: 5
-  console.log(myStack.pop()); // Output: Stack is empty. Cannot perform pop operation.
-  console.log(myStack.isEmpty()); // Output: true
+// 2. Pop Operations :
+
+  console.log(myStack.pop());                   // Output: 15
+  console.log(myStack.pop());                   // Output: 10
+
+// 3. isEmpty Operations :
+
+  console.log(myStack.isEmpty());               // Output: false
+
+  console.log(myStack.pop());                   // Output: 5
+  console.log(myStack.pop());                   // Output: Stack is empty. Cannot perform pop operation.
+  console.log(myStack.isEmpty());               // Output: true
   
