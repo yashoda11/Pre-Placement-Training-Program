b2bd0aa280a2dbc08b311eff3fcf424dc266c2c7
Yashoda <yashodalakshmi999@gmail.com>
2023-06-29 22:48:55 +0530

    Assignment - 18 Question - 06 Updated

--- DSA-Assignment/18-Assignment - Searching and Sorting/06-QUestion/script.js
+++ DSA-Assignment/18-Assignment - Searching and Sorting/06-QUestion/script.js
@@ -41,4 +41,3 @@ var findMinArrowShots = function (points) {
   var points = [[1, 2], [2, 3], [3, 4], [4, 5]];
   console.log("Example - 03 :");
   console.log(findMinArrowShots(points));                // Output: 2
-  
