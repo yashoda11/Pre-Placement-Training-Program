aff6f7c185be4f28032a9a5156bd1b26141cc4d0
Yashoda <yashodalakshmi999@gmail.com>
2023-07-09 11:27:03 +0530

    Assignment 20 Question - 02 Updated

--- DSA-Assignment/21-Assignment - Tree/01-Question/readme.md
+++ DSA-Assignment/21-Assignment - Tree/01-Question/readme.md
@@ -2,7 +2,8 @@
 ## **Question - 01**
 
 You are given a binary tree. The binary tree is represented using the TreeNode class. 
-- Each TreeNode has an integer value and left and right children, represented using the TreeNode class itself. - Convert this binary tree into a binary search tree.
+- Each TreeNode has an integer value and left and right children, represented using the TreeNode class itself. 
+- Convert this binary tree into a binary search tree.
 
 ```
 Example :
@@ -31,3 +32,14 @@ Output :
 
 2      7
 ```
+
+**Explanation :**
+1. The given code represents a solution to convert a binary tree into a binary search tree (BST). Let's go through the explanation of the code :
+2. The Node class represents a node in the binary tree. It has three properties: data to store the node's value, left to store the left child, and right to store the right child.
+3. The index variable is declared to keep track of the current index while performing operations on the array representation of the binary tree.
+4. The storeInorder function performs an inorder traversal of the binary tree and stores the values in the inorder array. It uses recursion to traverse the left subtree, store the current node's value, and then traverse the right subtree.
+5. The countNodes function recursively counts the number of nodes in the binary tree. It returns 0 for a null node and counts the nodes in the left and right subtrees by recursion.
+6. The arrayToBST function takes the arr (inorder traversal of the binary tree) and assigns the values to the binary search tree in a sorted manner. It uses recursion to assign the values to the left subtree, the current node, and then the right subtree.
+7. The binaryTreeToBST function is the main function that converts the given binary tree to a binary search tree. It calculates the number of nodes in the tree, creates an array to store the inorder traversal, performs the inorder traversal to populate the array, sorts the array, and assigns the values back to the binary tree using the arrayToBST function.
+8. The printInorder function performs an inorder traversal of the binary tree and prints the node values. It uses recursion to traverse the left subtree, prints the current node's value, and then traverses the right subtree.
+9. Finally, the code creates a sample binary tree with some nodes, calls the binaryTreeToBST function to convert it to a binary search tree, and prints the inorder traversal of the converted BST.
--- DSA-Assignment/21-Assignment - Tree/01-Question/script.js
+++ DSA-Assignment/21-Assignment - Tree/01-Question/script.js
@@ -1,81 +1,84 @@
-class TreeNode {
-    constructor(value) {
-      this.val = value;
-      this.left = null;
-      this.right = null;
-    }
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
   }
-  
-  function convertToBST(root) {
-    // Perform an in-order traversal of the binary tree to retrieve the values in sorted order
-    const values = [];
-    inOrderTraversal(root, values);
-  
-    // Create a new binary search tree using the sorted values
-    return constructBST(values);
+}
+
+let index = 0; // Define the index variable
+
+function storeInorder(node, inorder) {
+  if (node === null) {
+    return;
   }
-  
-  function inOrderTraversal(node, values) {
-    if (node === null) {
-      return;
-    }
-  
-    // Traverse the left subtree
-    inOrderTraversal(node.left, values);
-  
-    // Add the current node's value to the array
-    values.push(node.val);
-  
-    // Traverse the right subtree
-    inOrderTraversal(node.right, values);
+
+  storeInorder(node.left, inorder);
+
+  inorder[index] = node.data;
+  index++;
+
+  storeInorder(node.right, inorder);
+}
+
+function countNodes(root) {
+  if (root === null) {
+    return 0;
   }
-  
-  function constructBST(values) {
-    return buildTree(values, 0, values.length - 1);
+  return countNodes(root.left) + countNodes(root.right) + 1;
+}
+
+function arrayToBST(arr, root) {
+  if (root === null) {
+    return;
   }
-  
-  function buildTree(values, start, end) {
-    if (start > end) {
-      return null;
-    }
-  
-    // Find the middle element to make it the root of the current subtree
-    const mid = Math.floor((start + end) / 2);
-    const root = new TreeNode(values[mid]);
-  
-    // Recursively build the left and right subtrees
-    root.left = buildTree(values, start, mid - 1);
-    root.right = buildTree(values, mid + 1, end);
-  
-    return root;
+
+  arrayToBST(arr, root.left);
+
+  root.data = arr[index];
+  index++;
+
+  arrayToBST(arr, root.right);
+}
+
+function binaryTreeToBST(root) {
+  if (root === null) {
+    return;
   }
-  
-  // Construct the binary tree
-  const root = new TreeNode(10);
-  root.left = new TreeNode(2);
-  root.right = new TreeNode(7);
-  root.left.left = new TreeNode(8);
-  root.left.right = new TreeNode(4);
-  
-  // Convert the binary tree into a binary search tree
-  const bstRoot = convertToBST(root);
-  
-  // Print the binary search tree
-  console.log("Converted Binary Search Tree:");
-  printInOrderTraversal(bstRoot);
-  
-  function printInOrderTraversal(node) {
-    if (node === null) {
-      return;
-    }
-  
-    // Traverse the left subtree
-    printInOrderTraversal(node.left);
-  
-    // Print the current node's value
-    console.log(node.val);
-  
-    // Traverse the right subtree
-    printInOrderTraversal(node.right);
+
+  const n = countNodes(root);
+
+  const arr = new Array(n);
+
+  storeInorder(root, arr);
+
+  arr.sort((a, b) => a - b);
+
+  index = 0;
+  arrayToBST(arr, root);
+}
+
+function printInorder(node) {
+  if (node === null) {
+    return;
   }
-  
+
+  printInorder(node.left);
+
+  process.stdout.write(node.data + " ");
+
+  printInorder(node.right);
+}
+
+// Testing Example :
+
+const root = new Node(10);
+root.left = new Node(2);
+root.right = new Node(7);
+root.left.left = new Node(8);
+root.left.right = new Node(4);
+
+binaryTreeToBST(root);
+
+console.log("Following is the Inorder Traversal of the converted BST : ");
+printInorder(root);
