c82eb7546c0148f1905075b77efd25a7c3713729
Yashoda <yashodalakshmi999@gmail.com>
2023-07-12 13:25:07 +0530

    floders updated

--- Core Modules Test on Web-Development/Mock-Test - 02/01-Question/script.js
+++
@@ -1,9 +0,0 @@
-var squareBox = document.getElementById("box");
-
-squareBox.addEventListener("click" , (c) => {
-
-    squareBox.style.backgroundColor = "yellow";
-    squareBox.style.width = "600px";    // upon clicking the size (width) be doubled
-    squareBox.style.height = "600px";  // upon clicking the size (width) be doubled
-
-});
